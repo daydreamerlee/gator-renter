7af6b3fec12517dfd2184ec1681a9191eb158acd
Unknown <hassaanahmedrana@outlook.com>
2016-11-20 19:56:16 +0100

    Failsafe stored procedure structuring
    Modified the stored procedure to be fail safe and will serve as a template for future commits.

--- db_folder/Stored Procedures/2.0 - getUserDetail.sql
+++ db_folder/Stored Procedures/2.0 - getUserDetail.sql
@@ -1,8 +1,15 @@
-CREATE PROCEDURE `getUserDetail`(IN uid VARCHAR(25))
+USE `mini`;
+DROP procedure IF EXISTS `getUserDetail`;
+
+DELIMITER $$
+USE `mini`$$
+CREATE PROCEDURE `getUserDetail` (in uid varchar(25))
 BEGIN
 IF uid IS NULL THEN 
       Select * from users;
    ELSE
       Select * from users where uid = null;
    END IF;
-END
+END$$
+
+DELIMITER ;
