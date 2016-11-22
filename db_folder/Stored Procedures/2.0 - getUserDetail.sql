fa98f42723a1232dc065cbe670efd138a36227b0
Anil Manzoor <anilmanzoor88@yahoo.com>
2016-11-22 18:43:24 +0500

    Added non selection of inactive users to getUserDetail SP on all users fetch.

--- db_folder/Stored Procedures/2.0 - getUserDetail.sql
+++ db_folder/Stored Procedures/2.0 - getUserDetail.sql
@@ -6,7 +6,7 @@ USE `mini`$$
 CREATE PROCEDURE `getUserDetail` (in userid varchar(25))
 BEGIN
 IF userid IS NULL THEN 
-      Select * from users;
+      Select * from users where is_active <> 0;
    ELSE
       Select * from users where uid = userid and is_active <> 0;
    END IF;
