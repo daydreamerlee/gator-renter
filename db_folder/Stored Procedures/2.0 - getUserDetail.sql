bd8732d5d395c88273f0aef2492549427355aad4
Anil Manzoor <anilmanzoor88@yahoo.com>
2016-11-24 23:13:44 +0500

    Added info for user CRUD APIs. Changed SP to not return password hash in user details API.

--- db_folder/Stored Procedures/2.0 - getUserDetail.sql
+++ db_folder/Stored Procedures/2.0 - getUserDetail.sql
@@ -6,10 +6,11 @@ USE `mini`$$
 CREATE PROCEDURE `getUserDetail` (in userid varchar(25))
 BEGIN
 IF userid IS NULL THEN 
-      Select * from users where is_active <> 0;
+      Select uid, first_name, last_name, email, address, city, created, user_roles_id, is_active from users where is_active <> 0;
    ELSE
-      Select * from users where uid = userid and is_active <> 0;
+      Select uid, first_name, last_name, email, address, city, created, user_roles_id, is_active from users where uid = userid and is_active <> 0;
    END IF;
 END$$
 
 DELIMITER ;
+
