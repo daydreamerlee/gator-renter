de18154333f24de2809becfb36577b197c77bea9
Intesar Haider <intesar.haider1@gmail.com>
2016-11-26 23:40:27 +0100

    added queries

--- resources/db_folder/2.0 - internal_messages/queries.sql
+++
@@ -1,7 +0,0 @@
-select * from user_messages um
-left join users from_usr on (usr.id = um.from_user_id) 
-left join apartment on apt (apt.owner_user_id = ) where id in (
-	select max(um.id) from user_messages um
-	where um.to_user_id = 171
-	group by um.from_user_id, um.apartment_id 
-);
